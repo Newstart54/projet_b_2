f61f18be3dc865343c56709a971d5bf3de7e95b7
fabien charbonnelle <fcharbonnelle@gmail.com>
2024-10-22 15:21:14 +0200

    js

---
+++ exo.js
@@ -0,0 +1,16 @@
+//constantes
+
+const SeuilMajorationSalaire = 1800.00
+const MajorationBasSalaire = 0.10
+const TauxCelibataire = 0.20
+const TauxNonCelibataire = 0.25
+const TauxParEnfant = 0.15
+const TauxMaximalPart = 0.50
+
+//variables
+let NombreEnfants : entier
+let EstCélibataire : chaine
+let Rémunération : reel
+let TauxParticipation : reel
+
+//debut
